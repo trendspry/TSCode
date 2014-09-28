77411332c083dbda0e50f6ff72e77d6d6682f18b
rahul <rahul.agarwal@healthkart.com>
2014-09-28 17:57:56 +0530

    Tag printed

--- routes/updateDiscounts.js
+++ routes/updateDiscounts.js
@@ -1,6 +1,5 @@
-
 var express = require('express');
-var http = require('http');
+var request_json = require('request-json');
 var request = require("request");
 var router = express.Router();
 
@@ -10,6 +9,7 @@ var password = 'deb4069f2ffd14a1d0185b60de155e93';
 var passKey = apiKey + ':' + password;
 var baseUrl = 'trendspry-2.myshopify.com';
 var HTTPS = "https://";
+var postHost = HTTPS + passKey + '@' + baseUrl;
 var PAGE = "page="
 var pageNum = 1;
 
@@ -18,21 +18,12 @@ var pageNum = 1;
 router.get('/', function (req, res) {
 
 
-	var url = HTTPS + passKey + "@" + baseUrl + "/admin/variants.json?" + PAGE + pageNum;
+	var url = HTTPS + passKey + "@" + baseUrl + "/admin/products.json?" + PAGE + pageNum;
+	console.log(url)
 	reqeustForUrl(url);
 
 
-//	} while (continueParsing)
-
-	var text = '{"employees":[' +
-			'{"firstName":"John","lastName":"Doe" },' +
-			'{"firstName":"Rahul","lastName":"Smith" },' +
-			'{"firstName":"Peter","lastName":"Jones" }]}';
-
-	obj = JSON.parse(text);
-	console.log("after json parsing ");
-	var fullname = obj.employees[1].firstName + " " + obj.employees[1].lastName;
-
+	var fullname = "rahul agarwal";
 
 	res.render('helloworld', { title: fullname })
 });
@@ -53,12 +44,12 @@ function reqeustForUrl(url) {
 		if (!error && response.statusCode === 200) {
 			continueParsing = parseResponseForDiscount(body);
 			// Uncomment the below to let is work in a loop
-			/*if(continueParsing){
+			if(continueParsing){
 			 pageNum++;
-			 nextUrl = HTTPS + passKey + "@" + baseUrl + "/admin/variants.json?" + PAGE + pageNum;
+			 nextUrl = HTTPS + passKey + "@" + baseUrl + "/admin/products.json?" + PAGE + pageNum;
 			 console.log("next url " + nextUrl);
 			 reqeustForUrl(nextUrl);
-			 }*/
+			 }
 			console.log("continue parsing in func " + continueParsing);
 		}
 		else {
@@ -70,39 +61,76 @@ function reqeustForUrl(url) {
 
 function parseResponseForDiscount(body) {
 
-	var variantsJson = body;
+	var productsJson = body;
 	var compare_at_price = 0;
 	var price = 0;
 	var discount = 0;
 	var variantId = '';
 	var productId = '';
 	//print 'hel';
-	var variantsFound = variantsJson.variants.length;
+	var productsFound = productsJson.products.length;
 
 
-	console.log("variantsFound ==> " + variantsFound);
+	console.log("productsFound ==> " + productsFound);
 
-	if (variantsFound > 0) {
+	if (productsFound > 0) {
 
-		console.log(variantsJson.variants[0].price);
-		for (i = 0; i < variantsFound; i++) {
-
-			variantId = variantsJson.variants[i].id;
-			productId = variantsJson.variants[i].product_id;
-			compare_at_price = variantsJson.variants[i].compare_at_price;
-			price = variantsJson.variants[i].price;
-			if(compare_at_price > 0 && price > 0 && compare_at_price >= price){
+		var options;
+		var createDiscMetaString;
+		var headers;
+		var tags;
+		//console.log(productsJson.products[0].price);
+		//for (i = 0; i < 4; i++) {
+		for (i = 0; i < productsFound; i++) {
+			console.log("req no: " + (50*(pageNum-1)+i))
+			productId = productsJson.products[i].id;
+			compare_at_price = productsJson.products[i].variants[0].compare_at_price;
+			price = productsJson.products[i].variants[0].price;
+			tags = productsJson.products[i].tags;
+			if (compare_at_price > 0 && price > 0 && compare_at_price >= price) {
 				discount = (compare_at_price - price) * 100 / compare_at_price;
+				//discount = discount.toFixed(2);
 			}
 
+			// Json to create metadata field discount
+			createDiscMeta = { metafield: {
+				namespace: 'product',
+				key: 'discount',
+				value: discount.toFixed(2),
+				value_type: 'string'
+			}};
+
+			//createDiscMetaString = JSON.stringify(createDiscMeta);
+
+
+			var postUrl = '/admin/products/' + productId + '/metafields.json';
+			console.log(postHost + postUrl);
+
+			setTimeout(function(){
+
+				postDataToShopify(postHost, postUrl, createDiscMeta);
+			    },5000);
+
+
 			console.log("price:" + price + " compare_at_price:" + compare_at_price
-					+ " discount:" + discount.toFixed(2) + " pId:" + productId + " vId:" + variantId);
+					+ " discount:" + discount.toFixed(2) + " pId:" + productId + " tags:" + tags);
 		}
 
 
 		return true;
 	}
 	return false;
+}
+
+function postDataToShopify(postHost, postUrl, postData) {
+
+	var client = request_json.newClient(postHost);
+
+	client.post(postUrl, postData, function (err, res, body) {
+		console.log("inside post function");
+		return console.log(res.statusCode + JSON.stringify(body));
+	});
+
 }
 
 
