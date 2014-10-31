6ebc72ced0f5f2b3304918589bf271dfde17294f
rahul <rahul.agarwal@healthkart.com>
2014-10-31 19:16:26 +0530

    server.js

--- server.js
+++ server.js
@@ -1,69 +1,10 @@
-var express = require('express');
-var request = require('request');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+#!/usr/bin/env node
+var debug = require('debug')('rahulnode');
+var app = require('./app');
 
-var routes = require('./routes/index');
-var updateDiscounts = require('./routes/updateDiscounts');
-var updateOrderCodTags = require('./routes/updateOrderCodTags');
-//var updateUptoTags = require('./routes/updateUptoTags');
-var createDiscount = require('./routes/createDiscount');
-var shopifyTest = require('./routes/shopifyTest');
-var users = require('./routes/users');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/updateDiscounts', updateDiscounts);
-app.use('/updateOrderCodTags', updateOrderCodTags);
-app.use('/createDiscount', createDiscount);
-app.use('/shopifyTest', shopifyTest);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-module.exports = app;
+var server = app.listen(app.get('port'), app.get('ip'), function() {
+  debug('Express server listening on port ' + server.address().port);
+  });
