9d098f1d2d745c6b06f01e1d8495dc98a2ec336a
rahul <rahul.agarwal@healthkart.com>
2014-11-06 15:05:40 +0530

    test changes

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use('/shopifyTest', shopifyTest);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
